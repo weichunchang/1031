402370861722704d9982122830120aa01da410a0
weichunchang <1055450795@qq.com>
2017-11-01 22:20:21 +0800

    'change'

---
+++ src/pages/home/Header.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="header">
+		这是header
+	</div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style>
+	.header {
+		height:70px;
+		background: #00bcd4;
+	}
+</style>
